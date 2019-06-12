4cf29a51f1d764dd14152b4e95d0a166110fe7cb
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-12 16:59:52 +0200

    new student

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from Class import Student
 
 student1 = Student("Konrad", 23, "sound engineering", True)
+student2 = Student("Hidden", 99, "no exist", False)
 
-print(student1)
+print(student1.name)
+print(student2. name)
